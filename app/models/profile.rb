3b0631294260c924fdb79a2f86eab35178135eed
Winicius Reis <boi@Boi-UBUNTU-NOTE.(none)>
2012-08-04 12:01:47 -0300

    Some Profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,16 @@
 class Profile < ActiveRecord::Base
   attr_accessible :about, :birthday, :city, :college, :contact_email, :country,
                   :full_name, :gender, :high_school, :state, :website, :work_at
+  #Some fields validations 
+  validates :about,
+            :length => { :minimum =>3, :maximum => 500 }
+  validates :contact_email,
+            :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :full_name,
+            :presence => true,
+            :length => { :maximum => 40 }
+  validates :gender,
+            :inclusion => %w(male female)
+  #Associations          
   belongs_to :user
 end
